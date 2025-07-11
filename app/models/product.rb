d932224af3ba6fa9868075339241da615052773c
Ana Belén Bergese <anabelenbergese@MacBook-Pro.local>
2025-07-11 16:03:26 +0200

    Build: First commit. Generated model for Product, migrate DB, create controller & view for Product. Added 3 products to db

---
+++ app/models/product.rb
@@ -0,0 +1,5 @@
+class Product < ApplicationRecord
+    validates :code, presence: true
+    validates :name, presence: true
+    validates :price_cents, presence: true, numericality: { only_integer: true, greater_than: 0 }   
+end
