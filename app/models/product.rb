3d3fa1336265d49e834db739afaff80240922bac
Ana Belén Bergese <anabelenbergese@MacBook-Pro.local>
2025-07-11 21:56:23 +0200

    Refactor: whitespace was detected with rubocop and was deleted

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
     validates :code, presence: true, uniqueness: true
     validates :name, presence: true
-    validates :price_cents, presence: true, numericality: { only_integer: true, greater_than: 0 }   
+    validates :price_cents, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
