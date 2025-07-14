53e0030ded953a641ecf74c1538340db7a8dd4c5
Ana Belén Bergese <anabelenbergese@MacBook-Pro.local>
2025-07-14 12:31:58 +0200

    Build: added cors gem to connect with frontend. React running in port 3001 and backend in port 3000. A list of products is already displayed in react main app component

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,10 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+
+    respond_to do |format|
+      format.html # for the ruby view
+      format.json { render json: @products, only: [:code, :name, :price_cents] }
+    end
   end
 end
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Product List (from Rails)</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.code}>
+            {product.name} - €{(product.price_cents / 100).toFixed(2)}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
