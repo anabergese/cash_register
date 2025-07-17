4e19ecd14c84e300356ee8e40b3f0fa762e99be2
Ana Belén Bergese <anabelenbergese@MacBook-Pro.local>
2025-07-17 12:59:33 +0200

    Fix: Jest doesnt work properly with react-router-dom 7, so I installed react-router-dom 6 to be able to easily run tests. Note: this is problably because I used the command create-react-app that is deprecated

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { routes } from "./routes";
+import ProductList from "../product/ProductList";
+import CartDetails from "../cart/CartDetails";
+
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {routes.map((route, i) => (
-          <Route key={i} path={route.path} element={route.element} />
-        ))}
+        <Route path="/" element={<ProductList />} />
+        <Route path="/carts/:id" element={<CartDetails />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
